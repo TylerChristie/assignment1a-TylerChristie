ad25d825f0746d9135fa4c4007780804173cd946
TylerChristie <tjc6442@psu.edu>
2024-09-13 18:47:07 -0400

    Update write_ids.c

--- write_ids.c
+++ write_ids.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    char GITHUB_ID[] = "Fill_in_Your_Github_Id";
-    char STUDENT_ID[] = "Fill_in_Your_Student_Id";
+    char GITHUB_ID[] = "TylerChristie";
+    char STUDENT_ID[] = "965814634";
 
     return 0 ;
 }
